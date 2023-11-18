35f67bedb2f4524011df6a86c3519e6958a3a331
Tifenn-Lamure <tifenn.lamure@hotmail.com>
2023-11-19 00:27:36 +0100

    gestion de l'affichage du lien "modifier"

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/checkLogin.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/checkLogin.js
@@ -1,10 +1,13 @@
 const login = document.querySelector("#login");
 const logout = document.querySelector("#logout");
 
+const modifier = document.querySelector("#modifier");
+
 // fonction qui vérifie si un user est connecté
 function checkIfUserLogged() {
-    //par défaut aucun user est connecté
+    //par défaut aucun user est connecté : on cache le login et le lien modifier pour ouvrir la modale de modification
     logout.style.display = "none";
+    modifier.style.display = "none";
     //on regarde si un token a été enregistré
     const token = window.localStorage.getItem("token");
     //si on a un token...
@@ -13,6 +16,12 @@ function checkIfUserLogged() {
         logout.style.display = null;
         //on cache le login
         login.style.display = "none";
+        //on teste si le user connecté est le userId 1 (admin ?)
+        const userId = parseInt(window.localStorage.getItem("userId")); // parseInt pour convertir le string sauvegardé dans le localStorage en int
+        if(userId === 1) {
+            //on affiche le lien "modifier"
+            modifier.style.display = null;
+        }
     }
     else {
         //on affiche le login
